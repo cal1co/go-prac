9e91b8f06a79d0283de8fadaa574d585e9e1dd7c
Alex King <calicoalix@gmail.com>
2022-06-22 12:32:17 +1000

    complete 04

--- 04_arrays_and_slices/ArraysAndSlices.go
+++ 04_arrays_and_slices/ArraysAndSlices.go
@@ -101,4 +101,4 @@ func main() {
 // * cap function returns length of underlying array
 // * append function to add elements to slice
 // 	*-May cause expensive copy operation if underlying array is too small
-// * Copies refer to same underlying array
+// * Copies refer to same underlying array.
